e8df8f66c8580fcd4a41a6c34f6a2980e2f0470c
Bartosz Pajewski <bartoszpajewski@gmail.com>
2019-12-16 12:30:48 -0800

    Projekt.sh - naprawiono wciecia w kodzie - dodano blok umozliwiajacy instalacje oprogramowania - dodano gotowy plik do autostartu

--- projekt.sh
+++ projekt.sh
@@ -1,66 +1,102 @@
 #!/bin/bash
 
+if [ "$1" == "--install" ]
+then
+  echo "Install files $1";
+  echo "Prepare GRUB";
+  sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT=".*/GRUB_CMDLINE_LINUX_DEFAULT=\" quiet splash \"/' /etc/default/grub;
+  if [ $? -ne 0 ]
+  then
+    echo "SED error: $?";
+    exit 2;
+  fi
+  sed -i 's/^GRUB_CMDLINE_LINUX=".*/GRUB_CMDLINE_LINUX=\" quiet splash \"/' /etc/default/grub;
+  if [ $? -ne 0 ]
+  then
+    echo "SED error: $?";
+    exit 2;
+  fi
+  echo "Install flags";
+  cp -a flags/ $HOME/flags/
+  if [ $? -ne 0 ]
+  then
+    echo "CP error: $?";
+    exit 2;
+  fi
+  echo "Add autostart command";
+  cp gnome-terminal.desktop $HOME/.config/autostart
+  if [ $? -ne 0 ]
+  then
+    echo "CP error: $?";
+    exit 2;
+  fi
+  exit 0
+else
+  echo "Invalid argument";
+  exit 1;
+fi
+
 showmenu=$(more flags/showmenu.flag | grep -m1 '^[0-9]')
 switchcase=$(more flags/switchrun.flag | grep -m1 '^[0-9]')
 
-echo ${showmenu}
-
 if [ $showmenu -eq 1 ]
 then
-
-while :
-do
+  while [[ true ]]; do
     clear
     cat<<EOF
     ==============================
-    	   PROJEKT KIOSK
+       PROJEKT KIOSK
     ------------------------------
     Please enter your choice:
 
-    (0) Uruchomienie wybranego program 
-    (1) Uruchomienie wybranej prezentacji 
+    (0) Uruchomienie wybranego program
+    (1) Uruchomienie wybranej prezentacji
     (2) Uruchomienie wybranego filmu
     (3) Uruchomienie wybranego wygaszacza ekranu i muzyki w tle
-           (Q)uit
+    (Q)uit
     ------------------------------
 EOF
     read -n1 -s
     case "$REPLY" in
-    "0")  echo "Podaj nazwe programu wraz z parametrami uruchomienia go np. ls -l:";
-          sed -i 's/^[0-9]/0/' flags/switchrun.flag;
-	  sed -i '$ d' flags/cases/0.case;
-	  read appname; echo "$appname" >> flags/cases/0.case;
-	  sed -i 's/^[0-9]/0/' flags/showmenu.flag;;
-    "1")  echo "Podaj sciezke do pliku prezentacji";
-	  sed -i 's/^[0-9]/1/' flags/switchrun.flag;
-          sed -i '$ d' flags/cases/1.case;
-	  read filename; echo "$filename" >> flags/cases/1.case;
-	  sed -i 's/^[0-9]/0/' flags/showmenu.flag;;
-    "2")  echo "Podaj sciezke do filmu";
-	  sed -i 's/^[0-9]/2/' flags/switchrun.flag;
-	  sed -i '$ d' flags/cases/2.case;
-	  read filename; echo "$filename" >> flags/cases/2.case;
-          sed -i 's/^[0-9]/0/' flags/showmenu.flag;;
-    "3")  echo "Podaj sciezke do muzyki";
-          sed -i 's/^[0-9]/3/'flags/switchrun.flag;
-          sed -i '$ d' flags/cases/3.case; 
-	  read filename; echo "$filename" >> flags/cases/3.case;
-	  sed -i 's/^[0-9]/0/' flags/showmenu.flag;
-	  gnome-control-center background;;
-    "Q")  exit                      ;;
-    "q")  echo "case sensitive!!"   ;; 
-     * )  echo "invalid option"     ;;
+      "0" ) echo "Podaj nazwe programu wraz z parametrami uruchomienia go";
+      echo "Np.: ls -l";
+      sed -i 's/^[0-9]/0/' flags/switchrun.flag;
+      sed -i '$ d' flags/cases/0.case;
+      read appname; echo "$appname" >> flags/cases/0.case;
+      sed -i 's/^[0-9]/0/' flags/showmenu.flag;
+      shutdown -r now;;
+      "1" ) echo "Podaj sciezke do pliku prezentacji";
+      sed -i 's/^[0-9]/1/' flags/switchrun.flag;
+      sed -i '$ d' flags/cases/1.case;
+      read filename; echo "$filename" >> flags/cases/1.case;
+      sed -i 's/^[0-9]/0/' flags/showmenu.flag;
+      shutdown -r now;;
+      "2" ) echo "Podaj sciezke do filmu";
+      sed -i 's/^[0-9]/2/' flags/switchrun.flag;
+      sed -i '$ d' flags/cases/2.case;
+      read filename; echo "$filename" >> flags/cases/2.case;
+      sed -i 's/^[0-9]/0/' flags/showmenu.flag;
+      shutdown -r now;;
+      "3" ) echo "Podaj sciezke do muzyki";
+      sed -i 's/^[0-9]/3/'flags/switchrun.flag;
+      sed -i '$ d' flags/cases/3.case;
+      read filename; echo "$filename" >> flags/cases/3.case;
+      sed -i 's/^[0-9]/0/' flags/showmenu.flag;
+      gnome-control-center background;
+      echo "Nacisnij downolny klawisz, aby kontynuowac";
+      read p;
+      shutdown -r now;;
+      "Q" ) exit;;
+      "q" ) echo "Case Sensitive!";;
+       *  ) echo "Invalid option";;
     esac
     sleep 1
-done
-
+  done
 else
-    case "$switchcase" in
-    "0") $(sed -n 2p flags/cases/0.case);;
-    "1") libreoffice --impress --show $(sed -n 2p flags/cases/1.case);;
-    "2") ffplay "$(sed -n 2p flags/cases/2.case)" -fs;;
-    "3") gnome-screensaver-command -l; 
-         ffplay "$(sed -n 2p flags/cases/3.case)";;
+  case "$switchcase" in
+    "0" ) $(sed -n 2p flags/cases/0.case);;
+    "1" ) libreoffice --impress --show $(sed -n 2p flags/cases/1.case);;
+    "3" ) gnome-screensaver-command -l;
+     ffplay "$(sed -n 2p flags/cases3.case)";;
+  esac
 fi
-
-
